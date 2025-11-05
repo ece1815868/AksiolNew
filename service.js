26b6c210b15234c7b1ac86d3f39d49d3be943add
John pettas <ggpettas@gmail.com>
2025-11-05 11:30:15 +0200

    add windows service and new data in database

---
+++ service.js
@@ -0,0 +1,24 @@
+const Service = require('node-windows').Service;
+
+// Create a new service object
+const svc = new Service({
+  name: 'Aksiologiseis',                  // Service name
+  description: 'Εφαρμογή Αξιολογήσεωνno',
+  script: 'C:\\dev\\aksiol\\server.js', // Absolute path to your main app file
+  nodeOptions: [
+    '--harmony',
+    '--max_old_space_size=4096'
+  ],
+  // Optionally run as a specific user
+  // username: 'myuser',
+  // password: 'mypassword'
+});
+
+// Listen for "install" event
+svc.on('install', () => {
+  console.log('Service installed successfully!');
+  svc.start();
+});
+
+// Install the service
+svc.install();
